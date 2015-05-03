7c67ca97c69234cc1c403f2267402295d050b243
jimmay5469 <jimmay5469@gmail.com>
2015-05-02 20:11:27 -0400

    Only add auth header if token exists

--- addon/adapters/github.js
+++ addon/adapters/github.js
@@ -5,9 +5,11 @@ export default DS.RESTAdapter.extend({
   session: Ember.inject.service(),
   host: 'https://api.github.com',
   headers: Ember.computed('session.githubAccessToken', function() {
-    return {
-      Authorization: `token ${this.get('session.githubAccessToken')}`,
-    };
+    var headers = {};
+    if(this.get('session.githubAccessToken')) {
+      headers.Authorization = `token ${this.get('session.githubAccessToken')}`;
+    }
+    return headers;
   }),
   pathForType: function(type) {
     return Ember.String.camelize(Ember.String.pluralize(type.replace('github','')));
