2d71c30458984d1ae157c26e75d4312b4ec21ad6
jimmay5469 <jimmay5469@gmail.com>
2015-05-07 23:01:44 -0400

    Remove implicit inverse

--- addon/models/github-repository.js
+++ addon/models/github-repository.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
   fullName: DS.attr('string'),
   name: DS.attr('string'),
   description: DS.attr('string'),
-  owner: DS.belongsTo('githubUser', { async: true })
+  owner: DS.belongsTo('githubUser', {
+    async: true,
+    inverse: null
+  })
 });
