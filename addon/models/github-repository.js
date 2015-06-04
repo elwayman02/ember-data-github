d36d3e6d6a9600e607be1aefcef3c81703aadec9
Gaël Demette <demette.gael@gmail.com>
2015-06-04 18:56:19 +0200

    More informations

--- addon/models/github-repository.js
+++ addon/models/github-repository.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   fullName: DS.attr('string'),
   name: DS.attr('string'),
+  language: DS.attr('string'),
   description: DS.attr('string'),
   owner: DS.belongsTo('githubUser', {
     async: true,
@@ -12,5 +13,10 @@ export default DS.Model.extend({
     async: true,
     inverse: null
   }),
+  isFork: DS.attr('boolean'),
+  isPrivate: DS.attr('boolean'),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date'),
+  pushedAt: DS.attr('date'),
   branches: DS.hasMany('githubBranch', { async: true, })
 });
