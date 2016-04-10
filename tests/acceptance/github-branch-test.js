c5d59eeb5127daebbd6feb159014cf2a0eb92177
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-04-10 14:20:03 +0530

    Fixing GitHub branch acceptance tests

---
+++ tests/acceptance/github-branch-test.js
@@ -0,0 +1,57 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import startApp from 'dummy/tests/helpers/start-app';
+import Pretender from 'pretender';
+import Ember from 'ember';
+
+const { run } = Ember;
+
+let server, app, container, store;
+
+moduleForAcceptance('Acceptance | github branch', {
+  beforeEach() {
+    server = new Pretender();
+    server.prepareBody = function(body){ return JSON.stringify(body); };
+    app = startApp();
+    container = app.__container__;
+    store = run(container, 'lookup', 'service:store');
+  },
+
+  afterEach() {
+    server.shutdown();
+    run(app, app.destroy);
+    Ember.BOOTED = false;
+  }
+});
+
+test('finding a branch without authorization', function(assert) {
+  server.get('/repos/User1/Repository1/branches/Branch1', () => {
+    return [200, {}, Factory.build('branch')];
+  });
+
+  return run(() => {
+    return store.findRecord('githubBranch', 'User1/Repository1/branches/Branch1').then((branch) => {
+      assertGithubBranchOk(assert, branch);
+      assert.equal(store.peekAll('githubBranch').get('length'), 1);
+      assert.equal(server.handledRequests.length, 1);
+      assert.equal(server.handledRequests[0].requestHeaders.Authorization, undefined);
+    });
+  });
+});
+
+test('finding a branch', function(assert) {
+  container.lookup('service:github-session').set('githubAccessToken', 'abc123');
+  server.get('/repos/user1/repository1/branches/branch1', () => {
+    return [200, {}, Factory.build('branch')];
+  });
+
+  return run(() => {
+    return store.findRecord('githubBranch', 'user1/repository1/branches/branch1').then((branch) => {
+      assertGithubBranchOk(assert, branch);
+      assert.equal(store.peekAll('githubBranch').get('length'), 1);
+      assert.equal(server.handledRequests.length, 1);
+      assert.equal(server.handledRequests[0].requestHeaders.Authorization, 'token abc123');
+    });
+  });
+});
+
