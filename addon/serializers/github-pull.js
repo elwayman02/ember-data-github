ef837e76a30835327acf34216a03f94ca5ccd697
Chris Sedlmayr <chris@sedlmayr.co.uk>
2016-05-03 20:44:36 +0100

    Adds serializer for pull

---
+++ addon/serializers/github-pull.js
@@ -0,0 +1,24 @@
+import GithubSerializer from './github';
+
+export default GithubSerializer.extend({
+  normalize(modelClass, resourceHash, prop) {
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    let hash = {
+      id: resourceHash.id,
+      title: resourceHash.title,
+      state: resourceHash.state,
+      htmlUrl: resourceHash.html_url,
+      body: resourceHash.body,
+      createdAt: resourceHash.created_at,
+      updatedAt: resourceHash.updated_at,
+      closedAt: resourceHash.closed_at,
+      mergedAt: resourceHash.merged_at,
+      links: {
+        user: resourceHash.user.url,
+        assignee: resourceHash.assignee.url
+      }
+    };
+    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+    return this._super(modelClass, hash, prop);
+  }
+});
