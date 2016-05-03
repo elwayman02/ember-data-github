d54bbef7d0697acc808a3ff5ac95aa5d88a25c0c
Chris Sedlmayr <chris@sedlmayr.co.uk>
2016-05-03 20:44:23 +0100

    Adds model schema file for pulls

---
+++ addon/models/github-pull.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  state: DS.attr('string'),
+  htmlUrl: DS.attr('string'),
+  body: DS.attr('string'),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date'),
+  closedAt: DS.attr('date'),
+  mergedAt: DS.attr('date'),
+  user: DS.belongsTo('githubUser', {
+    async: true,
+    inverse: null
+  }),
+  assignee: DS.belongsTo('githubUser', {
+    async: true,
+    inverse: null
+  })
+});
