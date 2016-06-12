68d690650bc0d6761c77acb2e2365f354857be8f
Damian Senn <senn@puzzle.ch>
2016-06-12 13:03:00 +0200

    Add organization - user link

--- addon/models/github-organization.js
+++ addon/models/github-organization.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   login: DS.attr('string'),
   name: DS.attr('string'),
   avatarUrl: DS.attr('string'),
+  githubUsers: DS.hasMany('githubUsers', { async: true }),
   githubRepositories: DS.hasMany('githubRepository', { async: true })
 });
