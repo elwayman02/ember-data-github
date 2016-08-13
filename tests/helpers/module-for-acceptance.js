8cf3b8954d3505a456cde97564cf55f03450fe06
Jordan Hawker <hawker.jordan@gmail.com>
2016-08-12 20:53:29 -0700

    Update via `ember init` & add eslint

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,23 +1,23 @@
 import { module } from 'qunit';
+import RSVP from 'rsvp';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 
-export default function (name, options = {}) {
+const { Promise } = RSVP;
+
+export default function(name, options = {}) {
   module(name, {
     beforeEach() {
       this.application = startApp();
 
       if (options.beforeEach) {
-        options.beforeEach.call(this, ...arguments);
+        return options.beforeEach.call(this, ...arguments);
       }
     },
 
     afterEach() {
-      if (options.afterEach) {
-        options.afterEach.call(this, ...arguments);
-      }
-
-      destroyApp(this.application);
+      let afterEach = options.afterEach && options.afterEach.call(this, ...arguments);
+      return Promise.resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
 }
