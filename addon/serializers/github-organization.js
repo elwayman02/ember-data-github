5ccbdb2cee467718ae30229d4e72f30241dd5baf
Brian Runnells <brunnells@heroku.com>
2016-09-03 13:49:54 -0500

    remove old jscs rules

--- addon/serializers/github-organization.js
+++ addon/serializers/github-organization.js
@@ -2,7 +2,6 @@ import GithubSerializer from './github';
 
 export default GithubSerializer.extend({
   normalize(type, hash, prop) {
-    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
     hash = {
       id: hash.recordId || hash.login,
       login: hash.login,
@@ -13,7 +12,6 @@ export default GithubSerializer.extend({
         githubRepositories: hash.repos_url
       }
     };
-    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
     return this._super(type, hash, prop);
   }
 });
