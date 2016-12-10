410e1a571a0b0dc6ac665bf7b14019fb2f7941b3
Stephen Vance <steve@vance.com>
2016-12-10 00:10:06 -0500

    Tests for all adapter behaviors.

--- tests/unit/adapters/github-release-test.js
+++ tests/unit/adapters/github-release-test.js
@@ -10,3 +10,23 @@ test('it exists', function(assert) {
   let adapter = this.subject();
   assert.ok(adapter);
 });
+
+test('it builds the URL for the releases query correctly', function(assert) {
+  let adapter = this.subject();
+  const host = adapter.get('host');
+  const repo = 'jimmay5469/old-hash';
+
+  assert.equal(adapter.buildURL('github-release', null, null, 'query', { repo: repo }), `${host}/repos/${repo}/releases`);
+});
+
+test('it builds the URL for a specific release query correctly', function(assert) {
+  let adapter = this.subject();
+  const host = adapter.get('host');
+  const repo = 'jimmay5469/old-hash';
+  const release = 1;
+
+  assert.equal(
+    adapter.buildURL('github-release', null, null, 'queryRecord', { repo: repo, releaseId: release }),
+    `${host}/repos/${repo}/releases/${release}`
+  );
+});
