57a536e129dbf28c0d35c73df26832f8d93e3890
Andrew Appleton <andysapple@gmail.com>
2017-02-07 11:07:01 +0000

    Convert extracted links into pagination meta

--- addon/serializers/github.js
+++ addon/serializers/github.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
-const { isArray, String:Str } = Ember;
+const { isArray, String:Str, get, isNone } = Ember;
 
 export default DS.RESTSerializer.extend({
 
@@ -14,5 +14,34 @@ export default DS.RESTSerializer.extend({
     }
     wrappedPayload[fieldName] = payload;
     return this._super(store, primaryModelClass, wrappedPayload, id, requestType);
+  },
+
+  // Add metadata to the response for use with pagination. Formatted like:
+  //   {
+  //     first: { page: 1, per_page: 5 }
+  //     next:  { page: 3, per_page: 5 },
+  //     prev:  { page: 1, per_page: 5 },
+  //     last:  { page: 3, per_page: 5 }
+  //   }
+  //
+  extractMeta(store, modelClass, payload) {
+    const links = get(payload, `${Str.pluralize(modelClass.modelName)}.links`);
+
+    if (isNone(links)) {
+      return;
+    }
+
+    return Object.keys(links).reduce((meta, name) => {
+      const link = links[name];
+      const qs = link.split('?').pop();
+
+      meta[name] = qs.split('&').reduce((memo, str) => {
+        const [key, value] = str.split('=');
+        memo[key] = parseInt(value, 10);
+        return memo;
+      }, {});
+
+      return meta;
+    }, {});
   }
 });
