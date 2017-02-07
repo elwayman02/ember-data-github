74b1e65e6137b5520368d60d9082e222d1fbd837
Andrew Appleton <andysapple@gmail.com>
2017-02-07 10:33:02 +0000

    Extract links from response headers

--- addon/adapters/github.js
+++ addon/adapters/github.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 const { RESTAdapter } = DS;
-const { computed, inject } = Ember;
+const { computed, inject, isNone } = Ember;
 
 export default RESTAdapter.extend({
 
@@ -20,6 +20,39 @@ export default RESTAdapter.extend({
 
   pathForType(type) {
     return Ember.String.camelize(Ember.String.pluralize(type.replace('github', '')));
-  }
+  },
 
+  // Parse Link response header out into an object like:
+  //   {
+  //     first: 'https://api.github.com/resouce?page=1&per_page=5',
+  //     next:  'https://api.github.com/resouce?page=3&per_page=5',
+  //     prev:  'https://api.github.com/resouce?page=1&per_page=5',
+  //     last:  'https://api.github.com/resouce?page=4&per_page=5',
+  //   }
+  //
+  handleResponse(status, headers, payload, requestData) {
+    const linkHeader = headers.Link;
+    const result = this._super(status, headers, payload, requestData);
+    if (isNone(linkHeader)) {
+      return result;
+    }
+
+    const links = linkHeader.split(', ').reduce((memo, link) => {
+      let [url, rel] = link.split('; ');
+
+      try {
+        [, url] = url.match(/<(.+)>/);
+        [, rel] = rel.match(/rel=\"(.+)\"/);
+      } catch(error) {
+        // Any error in parsing should not cause the application to error
+        return;
+      }
+
+      memo[rel] = url;
+      return memo;
+    }, {});
+
+    result.links = links;
+    return result;
+  }
 });
