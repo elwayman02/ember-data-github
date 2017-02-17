efca0d092d50127239a2f8a2ce75e21671e7e2ab
Stephen Vance <steve@vance.com>
2017-02-17 12:33:39 -0500

    Silence the test warnings about `Factory` and test helpers by adding eslint global declarations.

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
   env: {
     'embertest': true
+  },
+  globals: {
+    'Factory': false,
+    'assertGithubUserOk':false,
+    'assertGithubRepositoryOk': false,
+    'assertGithubBranchOk': false,
+    'assertGithubReleaseOk': false,
+    'assertGithubOrganizationOk': false
   }
 };
