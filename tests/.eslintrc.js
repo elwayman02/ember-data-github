503aac516a0bfa3ae0423a9c048bdb02f2757e7d
Stephen Vance <steve@vance.com>
2017-02-24 18:22:17 -0500

    Remove lint global declarations.

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,12 +1,5 @@
 module.exports = {
   env: {
     'embertest': true
-  },
-  globals: {
-    'assertGithubUserOk':false,
-    'assertGithubRepositoryOk': false,
-    'assertGithubBranchOk': false,
-    'assertGithubReleaseOk': false,
-    'assertGithubOrganizationOk': false
   }
 };
