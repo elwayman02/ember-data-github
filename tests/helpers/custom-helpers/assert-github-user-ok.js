22c6f01ea675b412ee9204a6e70c5a17d7f026df
Stephen Vance <steve@vance.com>
2017-02-24 19:03:20 -0500

    Factor out an assertion builder utility for the rest of the assertion reworking.

--- tests/helpers/custom-helpers/assert-github-user-ok.js
+++ tests/helpers/custom-helpers/assert-github-user-ok.js
@@ -1,35 +1,21 @@
 import QUnit from 'qunit';
 import Ember from 'ember';
+import assertionBuilder from '../utils/defined-attribute-assertion-builder';
 
-QUnit.assert.githubUserOk = function(user, message) {
-  let allDefined = true;
-  let undefinedKeys = [];
-  [
-    'id',
-    'login',
-    'name',
-    'type',
-    'avatarUrl',
-    'publicRepos',
-    'publicGists',
-    'followers',
-    'following',
-    'createdAt',
-    'updatedAt',
-    'url'
-  ].forEach(key => {
-    if( user.get(key) === undefined ) {
-      allDefined = false;
-      undefinedKeys.push(key);
-    }
-  });
-  this.pushResult({
-    result: allDefined,
-    actual: allDefined,
-    expected: true,
-    message: `${message}: ${undefinedKeys.join(',')}`
-  });
-};
+QUnit.assert.githubUserOk = assertionBuilder([
+  'id',
+  'login',
+  'name',
+  'type',
+  'avatarUrl',
+  'publicRepos',
+  'publicGists',
+  'followers',
+  'following',
+  'createdAt',
+  'updatedAt',
+  'url'
+]);
 
 export default Ember.Test.registerHelper(
   'assertGithubUserOk',
