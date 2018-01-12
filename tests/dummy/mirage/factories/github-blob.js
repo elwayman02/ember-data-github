339ddbfcc0fcfe2e7296b91dbd742a789ce8c8cf
Brian Runnells <brian.runnells@gmail.com>
2018-01-12 15:05:01 -0600

    blob fixup

--- tests/dummy/mirage/factories/github-blob.js
+++ tests/dummy/mirage/factories/github-blob.js
@@ -1,4 +1,4 @@
-import { Factory, faker } from 'ember-cli-mirage';
+import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
   sha: '7fd1a60b01f91b314f59955a4e4d4e80d8edf11d',
