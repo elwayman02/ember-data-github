33c9f2a2c5c7702b34e809f9193ad2ed42089e3f
Brian Runnells <brian.runnells@gmail.com>
2018-01-13 12:54:26 -0600

    prefer find of map to find related records

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -7,20 +7,12 @@ export default function() {
 
   this.get('/repos/:owner/:repo/branches', ({ db: { githubBranches, githubRepositories } }, { params }) => {
     let repo = githubRepositories.where({ name: params.repo })[0];
-    return githubBranches.map((branch) => {
-      if (branch.repositoryId === repo.id) {
-        return branch;
-      }
-    });
+    return githubBranches.find(repo.branchIds);
   });
 
   this.get('/repos/:owner/:repo/releases', ({ db: { githubReleases, githubRepositories } }, { params }) => {
     let repo = githubRepositories.where({ name: params.repo })[0];
-    return githubReleases.map((release) => {
-      if (release.repositoryId === repo.id) {
-        return release;
-      }
-    });
+    return githubReleases.find(repo.releaseIds);
   });
 
   this.get('/repos/:owner/:repo', ({ db: { githubRepositories } }, { params }) => {
@@ -43,7 +35,7 @@ export default function() {
 
   this.get('user/repos', ({ db: { githubUsers, githubRepositories } }) => {
     let user = githubUsers[0]
-    let repos = githubRepositories.where({ ownerId: user.id });
+    let repos = githubRepositories.find(user.repositoryIds)
     repos.forEach((repo) => {
       repo.owner = user;
       return repo;
@@ -65,7 +57,7 @@ export default function() {
     return githubUsers[0];
   });
 
-  this.get('users', ({ db : { githubUsers } }) => {
+  this.get('users', ({ db: { githubUsers } }) => {
     return githubUsers;
   });
 
