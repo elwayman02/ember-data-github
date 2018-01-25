f1744c49ea438322a58f5ed1f44a2e499f91363b
Josh W Lewis <josh.w.lewis@gmail.com>
2018-01-25 14:45:40 -0600

    Remove extra newlines

--- addon/adapters/github-repository.js
+++ addon/adapters/github-repository.js
@@ -3,11 +3,9 @@ import GithubAdapter from './github';
 export default GithubAdapter.extend({
   urlForFindRecord(id, modelName, snapshot) {
     let builtURL = this._super(id, modelName, snapshot);
-
     if (Number.isInteger(id) === false) {
       builtURL = builtURL.replace('repositories', 'repos')
     }
-
     return builtURL.replace('%2F', '/');
   }
 });
